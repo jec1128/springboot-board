d0274a10891d248d151779310a75b47b69f40d7c
jec1128 <abcabc4567@naver.com>
2021-08-25 17:04:33 +0900

    2021.08.25 admin 삭제기능 보수

--- portfolio/src/main/java/com/ChoiSW/portfolio/controller/AdminController.java
+++ portfolio/src/main/java/com/ChoiSW/portfolio/controller/AdminController.java
@@ -50,5 +50,40 @@ public class AdminController {
         return "/admin/userList";
     }
 
+    @GetMapping("/boardList")
+    public String boardList(Model model, @PageableDefault(size = 10, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable,
+                           @RequestParam(required = false, defaultValue = "") String searchText){
+
+        Page<Board> boardList = boardRepository.findBoardByTitleContainingOrContentContaining(searchText,searchText, pageable);
+        int startPage = Math.max(1,boardList.getPageable().getPageNumber() - 5);
+        int endPage;
+        if(boardList.getTotalPages()==0){
+            endPage=1;
+        }
+        else {
+            endPage = Math.min(boardList.getTotalPages(), boardList.getPageable().getPageNumber() + 5);
+        }
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+        model.addAttribute("boardList",boardList);
+        return "/admin/boardList";
+    }
+
+    @DeleteMapping("/userDelete/{id}")
+    public ResponseEntity<?> userDelete(@PathVariable("id") Long userId){
+        User user =userRepository.findById(userId).orElse(null);
+        userRepository.delete(user);
+        System.out.println("삭제 성공");
+        return new ResponseEntity<>("{}", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/boardDelete/{id}")
+    public ResponseEntity<?> boardDelete(@PathVariable("id") Long boardId){
+        Board board =boardRepository.findById(boardId).orElse(null);
+        boardRepository.delete(board);
+        System.out.println("삭제 성공");
+        return new ResponseEntity<>("{}", HttpStatus.OK);
+    }
+
 
 }
